d5335a8f628c1a3a519c9d834369c3990d034cc0
dab <dbraband@gmail.com>
2010-11-01 11:41:37 +0100

    string optimziation

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ end
 
 # loop through each folder in 'public' and mount it at '/subfolder-name', e.g. '/css' etc.
 Dir.chdir('public') do
-  public_dirs = (Dir.glob("*").find_all{|entry| File::directory?(entry)}).collect{|dir| '/' + dir}
+  public_dirs = (Dir.glob("*").find_all{|entry| File::directory?(entry)}).collect{|dir| '/' << dir}
   use Rack::Static, :urls => public_dirs, :root => 'public'
 end
 
